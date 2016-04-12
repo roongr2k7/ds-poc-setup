1c94aa22178432b242544b688fd5fc5330f69c7e
Pitsanu Swangpheaw <pitsanu_s@hotmail.com>
2016-04-12 09:56:42 +0000

    add script for install java and dsc30(cassandra)

---
+++ install_cassandra.sh
@@ -0,0 +1,4 @@
+echo "deb http://debian.datastax.com/community stable main" | sudo tee -a /etc/apt/sources.list.d/cassandra.sources.list
+curl -L http://debian.datastax.com/debian/repo_key | sudo apt-key add -
+sudo apt-get update
+sudo apt-get install dsc30 cassandra cassandra-tools
