ab03cecad89b17b91ec0fedc33c32173f377b840
Pitsanu Swangpheaw <pitsanu_s@hotmail.com>
2016-07-15 17:06:48 +0700

    Update install_go.sh

--- install_go.sh
+++ install_go.sh
@@ -3,5 +3,6 @@
 sudo curl -O https://storage.googleapis.com/golang/go1.6.linux-amd64.tar.gz
 sudo tar -xvf go1.6.linux-amd64.tar.gz
 sudo mv go /usr/local
+echo 'export GOPATH=$HOME/go' >> ~/.profile
 echo 'export PATH=$PATH:/usr/local/go/bin' >> ~/.profile
 source ~/.profile
