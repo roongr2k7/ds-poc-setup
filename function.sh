be26ab507e38c8e804cb8c3b5a7834be453d113a
Pitsanu Swangpheaw <pitsanu_s@hotmail.com>
2016-12-22 14:41:27 +0700

    Update function.sh

--- function.sh
+++ function.sh
@@ -1,5 +1,16 @@
 #!/bin/bash
 
+function cp_with_backup {
+# src=$1, dest=$2
+  echo copy frile $1 to $2
+  if [[ -f $2 ]]; then
+    backup_name="$2`date +%Y%m%d%H%M%S`"
+    echo $2 already exist backup to $backup_name
+    mv $2 $backup_name
+  fi
+  cp $1 $2
+}
+
 function download {
 # url=$1
   if [[ -f `basename $1` ]]; then
